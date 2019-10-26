7dacc7c05d5e7d7ead48be34c9243a2c5ec112e4
fanyer <iofanyer@live.com>
2019-10-27 05:20:10 +0800

    update: uploader

--- src/main/java/com/example/demo/controller/UploadController.java
+++ src/main/java/com/example/demo/controller/UploadController.java
@@ -1,4 +1,40 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+@RestController
 public class UploadController {
+
+
+    @PostMapping("/upload")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+
+        System.out.println("user.dir : "+System.getProperty("user.dir"));
+
+        String path = System.getProperty("user.dir") + "/file/";
+
+        if(!new File(path).exists())   {
+            new File(path).mkdirs();
+        }
+
+        File newFile=new File(path+file.getOriginalFilename());
+//        //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
+        file.transferTo(newFile);
+//        long  endTime=System.currentTimeMillis();
+//        System.out.println("方法二的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        return "/success2";
+    }
+
+
 }
--- src/main/java/com/example/demo/service/StorageService.java
+++ src/main/java/com/example/demo/service/StorageService.java
@@ -1,4 +1,21 @@
 package com.example.demo.service;
 
-public interface Storage {
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface StorageService {
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    ServerProperties.Tomcat.Resource loadAsResource(String filename);
+
+    void deleteAll();
 }
